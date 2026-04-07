113f17dab8e6855318805947cfcaa66ec08d0cdc
davijsolimp2018-cell <davijsolimp2018@gmail.com>
2026-04-07 20:55:51 -0300

    M1

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -10,6 +10,7 @@ import {
   SheetHeader,
   SheetTitle,
 } from "@/components/ui/sheet"
+import { NotificationBell } from "@/components/notification-bell"
 
 interface MobileSidebarProps {
   onOpenLimitsInfo: () => void
@@ -57,6 +58,9 @@ export const MobileSidebar = forwardRef<MobileSidebarRef, MobileSidebarProps>(
                 </p>
               </div>
             </Button>
+
+            {/* Notification Bell */}
+            <NotificationBell variant="mobile" />
           </div>
         </SheetContent>
       </Sheet>
