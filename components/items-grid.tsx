a917cd81e0636b39aacb24ec59861b95df47b8b1
kahrcos2025q-droid <kahrcos2025q@gmail.com>
2026-04-08 17:49:49 -0300

    TESTE012222

--- components/items-grid.tsx
+++ components/items-grid.tsx
@@ -15,7 +15,7 @@ interface ItemsGridProps {
 }
 
 const ITEMS_PER_PAGE = 27
-const QUICK_ADD_COUNT = 20
+const QUICK_ADD_COUNT = 25
 
 export function ItemsGrid({ items, onOpenFriendCodeModal }: ItemsGridProps) {
   const [search, setSearch] = useState("")
